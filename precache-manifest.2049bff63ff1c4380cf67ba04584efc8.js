72e3ee99233a656ef3364f5b940b28339e56f190
azax25547 <momentumangular@gmail.com>
2019-01-07 13:30:39 +0530

    Updates

---
+++ precache-manifest.2049bff63ff1c4380cf67ba04584efc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "57103bfa522461b91169",
+    "url": "/static/js/main.57103bfa.chunk.js"
+  },
+  {
+    "revision": "7f5afc3b0fe4ae14da19",
+    "url": "/static/js/1.7f5afc3b.chunk.js"
+  },
+  {
+    "revision": "57103bfa522461b91169",
+    "url": "/static/css/main.6bf78674.chunk.css"
+  },
+  {
+    "revision": "64bc2eb1630aa2b6f68598fc838aa31a",
+    "url": "/index.html"
+  }
+];
